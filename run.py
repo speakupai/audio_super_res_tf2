4c00631475fa256cdd271050ccc55a07ce20ed99
Rana Ahmad <taimur1871@gmail.com>
2021-01-13 12:59:38 -0700

    rename temp

--- run.py
+++ run.py
@@ -1,22 +1,21 @@
-#!/bin/sh
-
-import os
-os.sys.path.append(os.path.abspath('.'))
-os.sys.path.append(os.path.dirname(os.path.abspath('.')))
-
-import matplotlib
-matplotlib.use('Agg')
-
-import argparse
-import numpy as np
-import pickle
-
+# new script to rewrite the model
 import tensorflow as tf
-from model import default_opt
-from in_out import load_h5, upsample_wav
-from audiotfilm import AudioTfilm
+import h5py
+import numpy as np
 
-# ----------------------------------------------------------------------------
+from in_out import load_h5
+import argparse
+
+from tensorflow.keras.layers import MaxPool1D, MaxPool2D, AveragePooling1D
+from tensorflow.keras.layers import Concatenate, Add
+from tensorflow.keras.layers import Activation, Dropout
+from tensorflow.keras.layers import Conv1D, UpSampling1D
+from tensorflow.keras.layers import LSTM, BatchNormalization, LeakyReLU
+
+from tensorflow.keras.initializers import RandomNormal, Orthogonal
+
+from utils import _apply_normalizer, _make_normalizer
+from layers import subpixel
 
 def make_parser():
   parser = argparse.ArgumentParser()
@@ -24,10 +23,6 @@ def make_parser():
 
   # train
   train_parser = subparsers.add_parser('train')
-  train_parser.set_defaults(func=train)
-
-  train_parser.add_argument('--model', default='audiotfilm',
-    choices=('audiotfilm'), help='only audiotfilm used')
   
   train_parser.add_argument('--train', required=True,
     help='path to h5 archive of training patches')
@@ -46,13 +41,7 @@ def make_parser():
   
   train_parser.add_argument('--layers', default=4, type=int,
     help='number of layers in each of the D and U halves of the network')
-  
-  train_parser.add_argument('--alg', default='adam',
-    help='optimization algorithm')
-  
-  train_parser.add_argument('--lr', default=1e-3, type=float,
-    help='learning rate')
-  
+    
   train_parser.add_argument('--r', type=int, default=4,
     help='upscaling factor')
   
@@ -63,109 +52,106 @@ def make_parser():
     help='size of pooling window')
   
   train_parser.add_argument('--strides', type=int, default=4, help='pooling stide')
-  
-  train_parser.add_argument('--full', default='false', choices=('true', 'false'))
 
-  # eval
-  eval_parser = subparsers.add_parser('eval')
-  eval_parser.set_defaults(func=eval)
-
-  eval_parser.add_argument('--logname', required=True,
-    help='path to training checkpoint')
-  
-  eval_parser.add_argument('--out-label', default='',
-    help='append label to output samples')
-  
-  eval_parser.add_argument('--wav-file-list', 
-    help='list of audio files for evaluation')
-  
-  eval_parser.add_argument('--r', help='upscaling factor', default = 4, type=int)
-  
-  eval_parser.add_argument('--sr', help='high-res sampling rate', type=int,
-    default=16000)
-  
-  eval_parser.add_argument('--grocery', default='false', choices=('true', 'false'))
-  
-  eval_parser.add_argument('--model', default='audiounet',
-    choices=('audiounet'), help='model to train')
-  
-  eval_parser.add_argument('--speaker', default='single', choices=('single', 'multi'),
-    help='number of speakers being trained on')
-  
-  eval_parser.add_argument('--pool_size', type=int, default=4,
-    help='size of pooling window')
-  
-  eval_parser.add_argument('--strides', type=int, default=4,
-    help='pooling stide')
-  
-  eval_parser.add_argument('--patch_size', type=int, default=8192,
-    help='Size of patches over which the model operates')
-  
   return parser
 
-# ----------------------------------------------------------------------------
+# ---------------------------------------------------------------------------------------------
+args = make_parser()
+get_inputs = vars(args.parse_args())
+print(get_inputs.keys())
 
-def train(args):
-  full = True if args.full == 'true' else False
+# get data, the audio data in this case is stored as h5 file
+X_train, Y_train = load_h5(get_inputs['train'])
+X_val, Y_val = load_h5(get_inputs['val'])
+
+# determine super-resolution level from the target audio
+n_dim, n_chan = Y_train[0].shape
+
+# get the upscaling factor 'r'
+r = get_inputs['r']
+assert n_chan == 1
+
+# get variables
+epochs = get_inputs['epochs']
+batch_size = get_inputs['batch_size']
+layers = get_inputs['layers']
+speaker = get_inputs['speaker']
+pool_size = get_inputs['pool_size']
+strides = get_inputs['strides']
+DRATE=2
+
+def audiotfilm(X_train, Y_train, layers):
+    inputs = tf.keras.Input(shape=(X_train.shape[1], X_train.shape[2]))
+    L = layers
+    n_filters = [  128,  256,  512, 512, 512, 512, 512, 512]
+    n_filtersizes = [65, 33, 17,  9,  9,  9,  9, 9, 9]
+    downsampling_l = []
+
+    x = inputs
+    # downsampling layers
+    for l, nf, fs in zip(range(L), n_filters, n_filtersizes):
+      with tf.name_scope('downsc_conv%d' % l):
+        x = (Conv1D(filters=nf, kernel_size=fs, dilation_rate=DRATE,
+            activation=None, padding='same', kernel_initializer='orthogonal'))(x)
+        x = (MaxPool1D(pool_size=2,padding='valid'))(x)
+        x = LeakyReLU(0.2)(x)
+
+        # create and apply the normalizer
+        nb = 128 / (2**l)
+        x_norm = _make_normalizer(x, nf, nb)
+        x = _apply_normalizer(x, x_norm, nf, nb)
+
+        downsampling_l.append(x)
+
+    # bottleneck layer
+    with tf.name_scope('bottleneck_conv'):
+        x = (Conv1D(filters=n_filters[-1], kernel_size=n_filtersizes[-1], dilation_rate=DRATE,
+            activation=None, padding='same', kernel_initializer='orthogonal'))(x)
+        x = (MaxPool1D(pool_size=2,padding='valid'))(x)
+        x = Dropout(0.5)(x)
+        x = LeakyReLU(0.2)(x)
+
+        # create and apply the normalizer
+        nb = 128 / (2**L)
+        x_norm = _make_normalizer(x, n_filters[-1], nb)
+        x = _apply_normalizer(x, x_norm, n_filters[-1], nb)
+
+    # upsampling layers
+    for l, nf, fs, l_in in (zip(range(L), n_filters, n_filtersizes, downsampling_l)):
+      with tf.name_scope('upsc_conv%d' % l):
+        # (-1, n/2, 2f)
+        x = (Conv1D(filters=2*nf, kernel_size=fs, dilation_rate=DRATE,
+            activation=None, padding='same', kernel_initializer='orthogonal'))(x)
+        
+        x = Dropout(0.5)(x)
+        x = Activation('relu')(x)
+        # (-1, n, f)
+        x = subpixel.SubPixel1D(x, r=2) 
+ 
+        # create and apply the normalizer
+        x_norm = _make_normalizer(x, nf, nb)
+        x = _apply_normalizer(x, x_norm, nf, nb)
+        # (-1, n, 2f)
+        x = Concatenate(axis=-1)([x, l_in])
+        print ('U-Block: ', x.get_shape())
+      
+      # final conv layer
+      with tf.name_scope('lastconv'):
+        x = Conv1D(filters=2, kernel_size=9, activation=None, 
+            padding='same', kernel_initializer='RandomNormal')(x)    
+        x = subpixel.SubPixel1D(x, r=2) 
+
+      g = Add()([x, X_train])
+      return g
+    outputs = tf.keras.layers.Conv1D(32, 3)(inputs)
+    model = tf.keras.Model(inputs=inputs, outputs=outputs)
+    model.compile(optimizer=tf.keras.optimizers.Adam(lr=1e-4,beta_1=0.9, beta_2=0.999), 
+    loss='binary_crossentorpy',
+     metrics='accuracy')
     
-  # get data
-  X_train, Y_train = load_h5(args.train)
-  X_val, Y_val = load_h5(args.val)
-  
-  # determine super-resolution level
-  n_dim, n_chan = Y_train[0].shape
-  r = Y_train[0].shape[1] / X_train[0].shape[1]
-  assert n_chan == 1
+    return model
 
-  # create model
-  model = get_model(args, n_dim, r, from_ckpt=False, train=True)
-  # train model
-  '''model.fit(X_train, Y_train, X_val, Y_val, epochs=args.epochs,
-            r=args.r, speaker=args.speaker, calc_full_snr = full)'''
-
-  model.fit(X_train, Y_train, epochs=args.epochs)
-
-def eval(args):
-  # load model
-  model = get_model(args, 0, args.r, from_ckpt=True, train=False)
-  model.load(args.logname) # from default checkpoint
-
-  if args.wav_file_list:
-    with open(args.wav_file_list) as f:
-      for line in f:
-        try:
-          print(line.strip())
-          if(args.speaker == 'single'):
-            upsample_wav('../data/vctk/VCTK-Corpus/wav48/p225/'
-                         +line.strip(), args, model)
-          else:
-            upsample_wav('../data/vctk/VCTK-Corpus/'
-                         +line.strip(), args, model)
-        except EOFError:
-          print ('WARNING: Error reading file:', line.strip())
-
-def get_model(args, n_dim, r, from_ckpt=False, train=True):
-  """Create a model based on arguments"""  
-  if train:
-    opt_params = { 'alg' : args.alg, 'lr' : args.lr, 'b1' : 0.9, 'b2' : 0.999,
-                   'batch_size': args.batch_size, 'layers': args.layers }
-  else: 
-    opt_params = default_opt
-
-  print(args.model)
-  # create model
-  if args.model == 'audiotfilm':
-    model = AudioTfilm(from_ckpt=from_ckpt, n_dim=n_dim, r=r,
-                              pool_size = args.pool_size, strides=args.strides,
-                              opt_params=opt_params, log_prefix=args.logname)  
-  else:
-    raise ValueError('Invalid model')
-  return model
-
-def main():
-  parser = make_parser()
-  args = parser.parse_args()
-  args.func(args)
-
-if __name__ == '__main__':
-  main()
+model = audiotfilm(X_train, Y_train, layers)
+model.compile()
+model.fit(X_train, Y_train)
+model.summary()
